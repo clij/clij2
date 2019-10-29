c10652a5afa5fa9204898f9417a23560499ab9cd
haesleinhuepf <rhaase@mpi-cbg.de>
2019-10-29 21:26:50 +0100

    better auto-completion for jython and groovy

--- src/main/java/net/haesleinhuepf/clij/jython/ScriptingAutoCompleteProvider.java
+++ src/main/java/net/haesleinhuepf/clij/jython/ScriptingAutoCompleteProvider.java
@@ -3,6 +3,7 @@ package net.haesleinhuepf.clij.jython;
 import ij.IJ;
 import ij.ImagePlus;
 import ij.gui.Roi;
+import ij.macro.Interpreter;
 import ij.process.ImageProcessor;
 import net.haesleinhuepf.clij.CLIJ;
 import net.haesleinhuepf.clij.clearcl.ClearCLBuffer;
@@ -16,10 +17,17 @@ import net.imglib2.img.display.imagej.ImageJFunctions;
 import net.imglib2.roi.Regions;
 import net.imglib2.view.Views;
 import org.fife.ui.autocomplete.BasicCompletion;
+import org.fife.ui.autocomplete.Completion;
 import org.fife.ui.autocomplete.DefaultCompletionProvider;
+import org.fife.ui.autocomplete.SortByRelevanceComparator;
 
+import javax.swing.text.BadLocationException;
+import javax.swing.text.JTextComponent;
 import java.lang.reflect.Method;
 import java.lang.reflect.Parameter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.RandomAccess;
 
 /**
@@ -32,6 +40,8 @@ import java.util.RandomAccess;
  */
 public class ScriptingAutoCompleteProvider extends DefaultCompletionProvider
 {
+    private final int maximumSearchResults = 100;
+
     private static ScriptingAutoCompleteProvider instance = null;
 
     public static ScriptingAutoCompleteProvider getInstance () {
@@ -128,6 +138,135 @@ public class ScriptingAutoCompleteProvider extends DefaultCompletionProvider
         }
 
         return new BasicCompletion(provider, headline, null, description);
+    }
+
+    @Override
+    public List<Completion> getCompletionByInputText(String inputText) {
+        inputText = inputText.toLowerCase();
+
+        ArrayList<Completion> result = new ArrayList<Completion>();
+
+        int count = 0;
+        int secondaryCount = 0;
+        for (Completion completion : completions) {
+            String text = completion.getInputText().toLowerCase();
+            if (text.contains(inputText)) {
+                if (text.startsWith(inputText)) {
+                    result.add(count, completion);
+                    count++;
+                } else {
+                    result.add(completion);
+                    secondaryCount++;
+                }
+            }
+            if (secondaryCount + count > maximumSearchResults) {
+                break; // if too many results are found, exit to not annoy the user
+            }
+        }
+
+        return result;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    @SuppressWarnings("unchecked")
+    protected List<Completion> getCompletionsImpl(JTextComponent comp) {
+
+        List<Completion> retVal = new ArrayList<Completion>();
+        String text = getAlreadyEnteredText(comp);
+
+        if (text != null) {
+            retVal = getCompletionByInputText(text);
+            appendMacroSpecificCompletions(text, retVal, comp);
+        }
+        return retVal;
+    }
+
+    private void appendMacroSpecificCompletions(String input, List<Completion> result, JTextComponent comp) {
+
+        List<Completion> completions = new ArrayList<Completion>();
+        String lcaseinput = input.toLowerCase();
+
+        String text = null;
+        try {
+            text = comp.getDocument().getText(0, comp.getDocument().getLength());
+        } catch (BadLocationException e) {
+            e.printStackTrace();
+            return;
+        }
+        text = text + "\n" + Interpreter.getAdditionalFunctions();
+
+        int linecount = 0;
+        String[] textArray = text.split("\n");
+        for (String line : textArray){
+            String trimmedline = line.trim();
+            String lcaseline = trimmedline.toLowerCase();
+            if (lcaseline.startsWith("function ") || lcaseline.startsWith("def ")) {
+                String command = trimmedline.substring(8).trim().replace("{", "");
+                String lcasecommand = command.toLowerCase();
+                if (lcasecommand.contains(lcaseinput)) {
+                    String description = findDescription(textArray, linecount, "User defined function " + command + "\n as specified in line " + (linecount + 1));
+
+
+                    completions.add(new BasicCompletion(this, command, null, description));
+                }
+            }
+            if (lcaseline.contains("=")) {
+                String command = trimmedline.substring(0, lcaseline.indexOf("=")).trim();
+                String lcasecommand = command.toLowerCase();
+                if (lcasecommand.contains(lcaseinput) && command.matches("[_a-zA-Z]+")) {
+                    String description = "User defined variable " + command + "\n as specified in line " + (linecount + 1);
+
+                    completions.add(new BasicCompletion(this, command, null, description));
+                }
+            }
+            linecount++;
+        }
+
+        Collections.sort(completions, new SortByRelevanceComparator());
+
+        result.addAll(0, completions);
+    }
+
+    private String findDescription(String[] textArray, int linecount, String defaultDescription) {
+        String resultDescription = "";
+        int l = linecount - 1;
+        while (l > 0) {
+            String lineBefore = textArray[l].trim();
+            System.out.println("Scanning B " + lineBefore);
+            if (lineBefore.startsWith("//")) {
+                resultDescription = lineBefore.substring(2) + "\n" + resultDescription;
+            } else {
+                break;
+            }
+            l--;
+        }
+        l = linecount + 1;
+        while (l < textArray.length - 1) {
+            String lineAfter = textArray[l].trim();
+            System.out.println("Scanning A " + lineAfter);
+            if (lineAfter.startsWith("//")) {
+                resultDescription = resultDescription + "\n" + lineAfter.substring(2);
+            } else {
+                break;
+            }
+            l++;
+        }
+        if (resultDescription.length() > 0) {
+            resultDescription = resultDescription + "<br><br>";
+        }
+        resultDescription = resultDescription + defaultDescription;
+
+        return resultDescription;
+    }
+
+
+    @Override
+    public List<Completion> getCompletions(JTextComponent comp) {
+        List<Completion> completions = this.getCompletionsImpl(comp);
+        return completions;
     }
 
     public static void main(String... args) {
