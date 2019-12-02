f3e2ffc1885a2ae3f70fff0cf93bb4c70f22fee6
Robert Haase <haesleinhuepf@users.noreply.github.com>
2019-12-02 08:24:11 +0100

    cleanup code, added comments

--- src/main/jython/with_statement.py
+++ src/main/jython/with_statement.py
@@ -3,6 +3,9 @@
 #
 # Using the with statement with clijx
 #
+# To try it out, activate the clij and 
+# clij2 update sites in Fiji
+#
 # Author: Robert Haase, rhaase@mpi-cbg.de
 #         December 2019
 #########################################
@@ -20,7 +23,6 @@ IJ.run(imp, "32-bit", "");
 imp.show();
 
 # init GPU
-
 with CLIJx.getInstance() as clijx:
 	# push image to GPU
 	input = clijx.push(imp);
@@ -33,6 +35,11 @@ with CLIJx.getInstance() as clijx:
 
 	# show result
 	clijx.show(blurred, "blurred");
+	
+	#
+	# By the end of this block, clijx will 
+	# clean up memory automatically
+	#
 
 # check if clijx was clean up by the end of the block
-clijx = CLIJx.getInstance().reportMemory();
+IJ.log(CLIJx.getInstance().reportMemory());
