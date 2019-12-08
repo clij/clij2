00161336d56becaac0685e640a569366c9a1272f
haesleinhuepf <rhaase@mpi-cbg.de>
2019-12-08 11:28:55 +0100

    Improved docs for replaceIntensities

--- src/main/java/net/haesleinhuepf/clijx/advancedfilters/ReplaceIntensities.java
+++ src/main/java/net/haesleinhuepf/clijx/advancedfilters/ReplaceIntensities.java
@@ -41,7 +41,12 @@ public class ReplaceIntensities extends AbstractCLIJxPlugin implements CLIJMacro
 
     @Override
     public String getDescription() {
-        return "Replaces specific intensities specified in a vector image with given new values specified in a vector image.";
+        return "Replaces integer intensities specified in a vector image. The vector image must be 3D with size (m, 1, 1) " +
+                "where m corresponds to the maximum intensity in the original image. Assuming the vector image contains values (0, 1, 0, 2) means: \n" +
+                " * All pixels with value 0 (first entry in the vector image) get value 0\n" +
+                " * All pixels with value 1 get value 1\n" +
+                " * All pixels with value 2 get value 0\n" +
+                " * All pixels with value 3 get value 2\n";
     }
 
     @Override
