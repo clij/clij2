799bcd61a6b80d3de3d38ddbbb851fa6d9d014ad
haesleinhuepf <rhaase@mpi-cbg.de>
2020-02-05 23:21:57 +0100

    added CLIJ2 javascript examples

---
+++ src/main/javascript/automaticThreshold.js
@@ -0,0 +1,32 @@
+// automaticThreshold.js
+// =====================
+//
+// Apply an automatic threshold method to an image on the GPU
+//
+// Author: Robert Haase, rhaase@mpi-cbg.de
+//         April 2019
+//////////////////////////////////////////
+importClass(Packages.ij.IJ);
+importClass(Packages.net.haesleinhuepf.clij2.CLIJ2);
+
+IJ.run("Close All");
+
+// load example image
+imp = IJ.openImage("http://wsr.imagej.net/images/blobs.gif");
+imp.show();
+
+// init GPU
+clij2 = CLIJ2.getInstance();
+
+// push image to GPU
+input = clij2.push(imp);
+
+// reserve memory for output, same size and type as input
+output = clij2.create(input);
+
+// apply threshold method on GPU
+clij2.automaticThreshold(input, output, "Otsu");
+
+// show result
+clij2.pullBinary(output).show();
+
