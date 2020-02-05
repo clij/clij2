164b624779d3d3f16b9f196cf6aa1d2eb844783d
haesleinhuepf <rhaase@mpi-cbg.de>
2020-02-05 23:21:40 +0100

    added CLIJ2 groovy examples

---
+++ src/main/groovy/automaticThreshold.groovy
@@ -0,0 +1,34 @@
+// automaticThreshold.groovy
+// =========================
+//
+// Apply an automatic threshold method to an image on the GPU
+//
+// Author: Robert Haase, rhaase@mpi-cbg.de
+//         April 2019
+//////////////////////////////////////////
+
+import ij.IJ;
+import net.haesleinhuepf.clij2.CLIJ2;
+
+
+IJ.run("Close All");
+
+// load example image
+imp = IJ.openImage("http://wsr.imagej.net/images/blobs.gif");
+imp.show();
+
+// init GPU
+clij2 = CLIJ2.getInstance();
+
+// push image to GPU
+input = clij2.push(imp);
+
+// reserve memory for output, same size and type as input
+output = clij2.create(input);
+
+// apply threshold method on GPU
+clij2.automaticThreshold(input, output, "Otsu");
+
+// show result
+clij2.pullBinary(output).show();
+
---
+++ src/main/groovy/maximumProjection.groovy
@@ -0,0 +1,33 @@
+// This script shows how generate a maximum Z projection using CLIJ and Groovy.
+//
+// Author: Robert Haase (@haesleinhuepf)
+// April 2019
+//
+////////////////////////////////////////
+
+import ij.IJ;
+import net.haesleinhuepf.clij2.CLIJ2;
+
+IJ.run("Close All");
+
+// Init GPU
+clij2 = CLIJ2.getInstance();
+
+// get some example data
+imp = IJ.openImage("http://imagej.nih.gov/ij/images/t1-head.zip");
+
+// create and fill memory in GPU
+imageInput = clij2.push(imp);
+long[]  dimensions = [imageInput.getWidth(), imageInput.getHeight()];
+imageOutput = clij2.create(dimensions, imageInput.getNativeType());
+
+// process the image
+clij2.maximumZProjection(imageInput, imageOutput);
+
+// show the result
+clij2.show(imageOutput, "output");
+
+// get the result back as variable
+result = clij2.pull(imageOutput);
+
+IJ.run("Enhance Contrast", "saturated=0.35");
