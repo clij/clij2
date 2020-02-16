55528424107ab4633153c13793db2436d44a3541
haesleinhuepf <rhaase@mpi-cbg.de>
2020-02-16 13:04:38 +0100

    bugfix: resample didn't work for 2D

--- src/main/java/net/haesleinhuepf/clij2/plugins/Resample.java
+++ src/main/java/net/haesleinhuepf/clij2/plugins/Resample.java
@@ -10,6 +10,7 @@ import net.haesleinhuepf.clij2.CLIJ2;
 import net.haesleinhuepf.clij2.AbstractCLIJ2Plugin;
 import net.haesleinhuepf.clij2.utilities.CLIJUtilities;
 import net.imglib2.realtransform.AffineTransform3D;
+import net.imglib2.realtransform.AffineTransform2D;
 import org.scijava.plugin.Plugin;
 
 /**
@@ -31,7 +32,16 @@ public class Resample extends AbstractCLIJ2Plugin implements CLIJMacroPlugin, CL
         return result;
     }
 
+    @Deprecated
     public static boolean resample(CLIJ2 clij2, ClearCLImageInterface input, ClearCLImageInterface output, Float factorX, Float factorY, Float factorZ, Boolean linearInterpolation) {
+        if (input.getDimension() == 3) {
+            return resample3D(clij2, input, output, factorX, factorY, factorZ, linearInterpolation);
+        } else {
+            return resample2D(clij2, input, output, factorX, factorY, linearInterpolation);
+        }
+    }
+
+    public static boolean resample3D(CLIJ2 clij2, ClearCLImageInterface input, ClearCLImageInterface output, Float factorX, Float factorY, Float factorZ, Boolean linearInterpolation) {
         AffineTransform3D at = new AffineTransform3D();
         at.set(factorX, 0, 0, 0,
                 0, factorY, 0, 0,
@@ -58,6 +68,39 @@ public class Resample extends AbstractCLIJ2Plugin implements CLIJMacroPlugin, CL
                 // TODO: fix that workaround for an inconvenient API of affineTransform3D
                 ClearCLBuffer temp = clij2.create(output.getDimensions(), output.getNativeType());
                 clij2.affineTransform3D((ClearCLBuffer) input, temp, at);
+                clij2.copy(temp, output);
+                clij2.release(temp);
+            }
+        }
+        return true;
+    }
+
+    public static boolean resample2D(CLIJ2 clij2, ClearCLImageInterface input, ClearCLImageInterface output, Float factorX, Float factorY, Boolean linearInterpolation) {
+        AffineTransform2D at = new AffineTransform2D();
+        at.set(factorX, 0, 0,
+                0, factorY, 0);
+
+        if (linearInterpolation) {
+            ClearCLImage inputImage = clij2.create(input.getDimensions(), CLIJUtilities.nativeToChannelType(input.getNativeType()));
+            clij2.copy(input, inputImage);
+            ClearCLImage outputImage = clij2.create(output.getDimensions(), CLIJUtilities.nativeToChannelType(output.getNativeType()));
+
+            clij2.affineTransform2D(inputImage, outputImage, at);
+            inputImage.close();
+
+            clij2.copy(outputImage, output);
+            outputImage.close();
+        } else {
+            if (input instanceof ClearCLBuffer && output instanceof ClearCLBuffer) {
+                clij2.affineTransform2D((ClearCLBuffer) input, (ClearCLBuffer) output, at);
+            } else if(input instanceof ClearCLImage && output instanceof ClearCLBuffer) {
+                clij2.affineTransform2D((ClearCLImage) input, (ClearCLBuffer) output, at);
+            } else if(input instanceof ClearCLImage && output instanceof ClearCLImage) {
+                clij2.affineTransform2D((ClearCLImage) input, (ClearCLImage) output, at);
+            } else if(input instanceof ClearCLBuffer && output instanceof ClearCLImage) {
+                // TODO: fix that workaround for an inconvenient API of affineTransform3D
+                ClearCLBuffer temp = clij2.create(output.getDimensions(), output.getNativeType());
+                clij2.affineTransform2D((ClearCLBuffer) input, temp, at);
                 clij2.copy(temp, output);
                 clij2.release(temp);
             }
