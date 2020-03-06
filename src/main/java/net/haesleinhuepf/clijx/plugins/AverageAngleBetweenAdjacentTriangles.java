79083110e96dc683c3063eda63f8b8ee79aa8b84
haesleinhuepf <rhaase@mpi-cbg.de>
2020-03-06 13:09:11 +0100

    moved averageAngleBetweenAdjacentTriangles from CLIJ2 to CLIJx

--- src/main/java/net/haesleinhuepf/clijx/plugins/AverageAngleBetweenAdjacentTriangles.java
+++ src/main/java/net/haesleinhuepf/clijx/plugins/AverageAngleBetweenAdjacentTriangles.java
@@ -8,12 +8,14 @@ import net.haesleinhuepf.clij.macro.CLIJOpenCLProcessor;
 import net.haesleinhuepf.clij.macro.documentation.OffersDocumentation;
 import net.haesleinhuepf.clij2.AbstractCLIJ2Plugin;
 import net.haesleinhuepf.clij2.CLIJ2;
+import net.haesleinhuepf.clijx.CLIJx;
+import net.haesleinhuepf.clijx.utilities.AbstractCLIJxPlugin;
 import org.scijava.plugin.Plugin;
 
 import java.util.HashMap;
 
-@Plugin(type = CLIJMacroPlugin.class, name = "CLIJ2_averageAngleBetweenAdjacentTriangles")
-public class AverageAngleBetweenAdjacentTriangles extends AbstractCLIJ2Plugin implements CLIJMacroPlugin, CLIJOpenCLProcessor, OffersDocumentation {
+@Plugin(type = CLIJMacroPlugin.class, name = "CLIJx_averageAngleBetweenAdjacentTriangles")
+public class AverageAngleBetweenAdjacentTriangles extends AbstractCLIJxPlugin implements CLIJMacroPlugin, CLIJOpenCLProcessor, OffersDocumentation {
 
     @Override
     public String getParameterHelpText() {
@@ -23,12 +25,12 @@ public class AverageAngleBetweenAdjacentTriangles extends AbstractCLIJ2Plugin im
     @Override
     public boolean executeCL() {
         Object[] args = openCLBufferArgs();
-        boolean result = averageAngleBetweenAdjacentTriangles(getCLIJ2(), (ClearCLBuffer) (args[0]), (ClearCLBuffer) (args[1]),  (ClearCLBuffer) (args[2]));
+        boolean result = averageAngleBetweenAdjacentTriangles(getCLIJx(), (ClearCLBuffer) (args[0]), (ClearCLBuffer) (args[1]),  (ClearCLBuffer) (args[2]));
         releaseBuffers(args);
         return result;
     }
 
-    public static boolean averageAngleBetweenAdjacentTriangles(CLIJ2 clij2, ClearCLBuffer pointlist, ClearCLBuffer touch_matrix, ClearCLBuffer dst_average_angle_list) {
+    public static boolean averageAngleBetweenAdjacentTriangles(CLIJx clijx, ClearCLBuffer pointlist, ClearCLBuffer touch_matrix, ClearCLBuffer dst_average_angle_list) {
 
         HashMap<String, Object> parameters = new HashMap<String, Object>();
         parameters.put("src_pointlist", pointlist);
@@ -37,8 +39,8 @@ public class AverageAngleBetweenAdjacentTriangles extends AbstractCLIJ2Plugin im
 
         long[] globalSizes = new long[]{touch_matrix.getWidth()};
 
-        clij2.activateSizeIndependentKernelCompilation();
-        clij2.execute(AverageAngleBetweenAdjacentTriangles.class, "average_angle_between_adjacent_triangles_x.cl", "average_angle_between_adjacent_triangles", globalSizes, globalSizes, parameters);
+        clijx.activateSizeIndependentKernelCompilation();
+        clijx.execute(AverageAngleBetweenAdjacentTriangles.class, "average_angle_between_adjacent_triangles_x.cl", "average_angle_between_adjacent_triangles", globalSizes, globalSizes, parameters);
 
         return true;
     }
