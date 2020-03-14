12ece9d99824e9ef99c24f4f6aa1c56227a56766
haesleinhuepf <rhaase@mpi-cbg.de>
2020-03-14 18:24:04 +0100

    added invalidateKernelCache operation

---
+++ src/main/java/net/haesleinhuepf/clij2/plugins/InvalidateKernelCache.java
@@ -0,0 +1,39 @@
+package net.haesleinhuepf.clij2.plugins;
+
+import ij.IJ;
+import net.haesleinhuepf.clij.macro.CLIJMacroPlugin;
+import net.haesleinhuepf.clij.macro.CLIJOpenCLProcessor;
+import net.haesleinhuepf.clij.macro.documentation.OffersDocumentation;
+import net.haesleinhuepf.clij2.AbstractCLIJ2Plugin;
+import net.haesleinhuepf.clij2.CLIJ2;
+import org.scijava.plugin.Plugin;
+
+/**
+ * Author: @haesleinhuepf
+ * 12 2018
+ */
+
+@Plugin(type = CLIJMacroPlugin.class, name = "CLIJ2_invalidateKernelCache")
+public class InvalidateKernelCache extends AbstractCLIJ2Plugin implements CLIJMacroPlugin, CLIJOpenCLProcessor, OffersDocumentation {
+
+    @Override
+    public boolean executeCL() {
+        getCLIJ2().invalidateKernelCahe();
+        return true;
+    }
+
+    @Override
+    public String getParameterHelpText() {
+        return "";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Invalidated all cached OpenCL programs and kernels.";
+    }
+
+    @Override
+    public String getAvailableForDimensions() {
+        return "";
+    }
+}
