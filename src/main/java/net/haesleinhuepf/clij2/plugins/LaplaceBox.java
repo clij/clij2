535d84fba4d896748d3bcc1598602d76d06b60b7
haesleinhuepf <rhaase@mpi-cbg.de>
2020-03-14 18:23:11 +0100

    translated laplace filter to CLIJ2

--- src/main/java/net/haesleinhuepf/clij2/plugins/LaplaceBox.java
+++ src/main/java/net/haesleinhuepf/clij2/plugins/LaplaceBox.java
@@ -7,12 +7,14 @@ import net.haesleinhuepf.clij.macro.AbstractCLIJPlugin;
 import net.haesleinhuepf.clij.macro.CLIJMacroPlugin;
 import net.haesleinhuepf.clij.macro.CLIJOpenCLProcessor;
 import net.haesleinhuepf.clij.macro.documentation.OffersDocumentation;
+import net.haesleinhuepf.clij2.AbstractCLIJ2Plugin;
+import net.haesleinhuepf.clij2.CLIJ2;
 import org.scijava.plugin.Plugin;
 
 import java.util.HashMap;
 
 @Plugin(type = CLIJMacroPlugin.class, name = "CLIJ2_laplaceBox")
-public class LaplaceBox extends AbstractCLIJPlugin implements CLIJMacroPlugin, CLIJOpenCLProcessor, OffersDocumentation {
+public class LaplaceBox extends AbstractCLIJ2Plugin implements CLIJMacroPlugin, CLIJOpenCLProcessor, OffersDocumentation {
 
     @Override
     public String getParameterHelpText() {
@@ -24,16 +26,17 @@ public class LaplaceBox extends AbstractCLIJPlugin implements CLIJMacroPlugin, C
         ClearCLBuffer input = (ClearCLBuffer) (args[0]);
         ClearCLBuffer output = (ClearCLBuffer) (args[1]);
 
-        boolean result = laplaceBox(clij, input, output);
+        boolean result = laplaceBox(getCLIJ2(), input, output);
         return result;
     }
 
 
-    public static boolean laplaceBox(CLIJ clij, ClearCLBuffer src, ClearCLBuffer dst) {
+    public static boolean laplaceBox(CLIJ2 clij2, ClearCLBuffer src, ClearCLBuffer dst) {
         HashMap<String, Object> parameters = new HashMap<>();
         parameters.put("src", src);
         parameters.put("dst", dst);
-        return clij.execute(LaplaceBox.class, "laplace_box_" + src.getDimension() + "d_x.cl", "laplace_box_" + src.getDimension() + "d", dst.getDimensions(), parameters);
+        clij2.execute(LaplaceBox.class, "laplace_box_" + src.getDimension() + "d_x.cl", "laplace_box_" + src.getDimension() + "d", dst.getDimensions(), dst.getDimensions(), parameters);
+        return true;
     }
 
 
--- src/main/java/net/haesleinhuepf/clij2/plugins/LaplaceDiamond.java
+++ src/main/java/net/haesleinhuepf/clij2/plugins/LaplaceDiamond.java
@@ -7,12 +7,14 @@ import net.haesleinhuepf.clij.macro.AbstractCLIJPlugin;
 import net.haesleinhuepf.clij.macro.CLIJMacroPlugin;
 import net.haesleinhuepf.clij.macro.CLIJOpenCLProcessor;
 import net.haesleinhuepf.clij.macro.documentation.OffersDocumentation;
+import net.haesleinhuepf.clij2.AbstractCLIJ2Plugin;
+import net.haesleinhuepf.clij2.CLIJ2;
 import org.scijava.plugin.Plugin;
 
 import java.util.HashMap;
 
 @Plugin(type = CLIJMacroPlugin.class, name = "CLIJ2_laplaceDiamond")
-public class LaplaceDiamond extends AbstractCLIJPlugin implements CLIJMacroPlugin, CLIJOpenCLProcessor, OffersDocumentation {
+public class LaplaceDiamond extends AbstractCLIJ2Plugin implements CLIJMacroPlugin, CLIJOpenCLProcessor, OffersDocumentation {
 
     @Override
     public String getParameterHelpText() {
@@ -24,16 +26,17 @@ public class LaplaceDiamond extends AbstractCLIJPlugin implements CLIJMacroPlugi
         ClearCLBuffer input = (ClearCLBuffer) (args[0]);
         ClearCLBuffer output = (ClearCLBuffer) (args[1]);
 
-        boolean result = laplaceSphere(clij, input, output);
+        boolean result = laplaceSphere(getCLIJ2(), input, output);
         return result;
     }
 
 
-    public static boolean laplaceSphere(CLIJ clij, ClearCLBuffer src, ClearCLBuffer dst) {
+    public static boolean laplaceSphere(CLIJ2 clij2, ClearCLBuffer src, ClearCLBuffer dst) {
         HashMap<String, Object> parameters = new HashMap<>();
         parameters.put("src", src);
         parameters.put("dst", dst);
-        return clij.execute(LaplaceDiamond.class, "laplace_diamond_" + src.getDimension() + "d.cl", "laplace_diamond_" + src.getDimension() + "d", dst.getDimensions(), parameters);
+        clij2.execute(LaplaceDiamond.class, "laplace_diamond_" + src.getDimension() + "d.cl", "laplace_diamond_" + src.getDimension() + "d", dst.getDimensions(), dst.getDimensions(), parameters);
+        return true;
     }
 
 
