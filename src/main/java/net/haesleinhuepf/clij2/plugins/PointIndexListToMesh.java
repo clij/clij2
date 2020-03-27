85708a1d4f1e85377ca28112fbbd87842ec36a68
haesleinhuepf <rhaase@mpi-cbg.de>
2020-03-27 19:50:29 +0100

    bugfix: Mesh image initialisation code was missing

--- src/main/java/net/haesleinhuepf/clij2/plugins/PointIndexListToMesh.java
+++ src/main/java/net/haesleinhuepf/clij2/plugins/PointIndexListToMesh.java
@@ -1,6 +1,7 @@
 package net.haesleinhuepf.clij2.plugins;
 
 import net.haesleinhuepf.clij.clearcl.ClearCLBuffer;
+import net.haesleinhuepf.clij.coremem.enums.NativeTypeEnum;
 import net.haesleinhuepf.clij.macro.CLIJMacroPlugin;
 import net.haesleinhuepf.clij.macro.CLIJOpenCLProcessor;
 import net.haesleinhuepf.clij.macro.documentation.OffersDocumentation;
@@ -8,6 +9,7 @@ import net.haesleinhuepf.clij2.AbstractCLIJ2Plugin;
 import net.haesleinhuepf.clij2.CLIJ2;
 import org.scijava.plugin.Plugin;
 
+import java.nio.ShortBuffer;
 import java.util.HashMap;
 
 /**
@@ -23,7 +25,7 @@ public class PointIndexListToMesh extends AbstractCLIJ2Plugin implements CLIJMac
 
     @Override
     public String getParameterHelpText() {
-        return "Image pointlist, Image indexList, Image Mesh";
+        return "Image pointlist, Image indexList, ByRef Image mesh_destination";
     }
 
     @Override
@@ -45,6 +47,29 @@ public class PointIndexListToMesh extends AbstractCLIJ2Plugin implements CLIJMac
         clij2.activateSizeIndependentKernelCompilation();
         clij2.execute(PointIndexListToMesh.class, "pointindexlist_to_mesh_3d_x.cl", "pointindexlist_to_mesh_3d", dimensions, dimensions, parameters);
         return true;
+    }
+
+    @Override
+    public ClearCLBuffer createOutputBufferFromSource(ClearCLBuffer input) {
+        ClearCLBuffer pointlist = (ClearCLBuffer) args[0];
+
+        CLIJ2 clij2 = getCLIJ2();
+        ClearCLBuffer temp = clij2.create(new long[]{1, pointlist.getHeight()}, NativeTypeEnum.UnsignedShort);
+
+        clij2.maximumXProjection(input, temp);
+
+        short[] array = new short[(int) temp.getHeight()];
+        ShortBuffer buffer = ShortBuffer.wrap(array);
+
+        temp.writeTo(buffer, true);
+        clij2.release(temp);
+
+        long[] dimensions = new long[array.length];
+        for (int d = 0; d < array.length; d++) {
+            dimensions[d] = array[d];
+        }
+
+        return clij2.create(dimensions, input.getNativeType());
     }
 
     @Override
