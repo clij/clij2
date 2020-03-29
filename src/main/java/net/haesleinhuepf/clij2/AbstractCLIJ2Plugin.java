3c0821c024bb3e01ca883a8f1a995b800665bffc
haesleinhuepf <rhaase@mpi-cbg.de>
2020-03-29 22:44:31 +0200

    bugfix: enable plugins running on different GPUs at a time

--- src/main/java/net/haesleinhuepf/clij2/AbstractCLIJ2Plugin.java
+++ src/main/java/net/haesleinhuepf/clij2/AbstractCLIJ2Plugin.java
@@ -1,10 +1,34 @@
 package net.haesleinhuepf.clij2;
 
+import net.haesleinhuepf.clij.CLIJ;
 import net.haesleinhuepf.clij.macro.AbstractCLIJPlugin;
 
 public abstract class AbstractCLIJ2Plugin extends AbstractCLIJPlugin {
+
+    private CLIJ2 clij2;
+
+    @Override
+    public void setClij(CLIJ clij) {
+        if (clij2 == null || clij2.getCLIJ() != clij) {
+            if (clij == CLIJ.getInstance()) {
+                clij2 = CLIJ2.getInstance();
+            } else {
+                System.out.println("Warning AbstractCLIJ2Plugin.setCLIJ is deprecated. Use setCLIJ2 instead.");
+                clij2 = new CLIJ2(clij);
+            }
+        }
+        super.setClij(clij);
+    }
+
+    public void setCLIJ2(CLIJ2 clij2) {
+        this.clij2 = clij2;
+    }
+
     public CLIJ2 getCLIJ2() {
-        // Note: The internals here may change
-        return CLIJ2.getInstance();
+        return clij2;
+    }
+
+    public static Integer asInteger(Object number) {
+        return AbstractCLIJPlugin.asInteger(number);
     }
 }
