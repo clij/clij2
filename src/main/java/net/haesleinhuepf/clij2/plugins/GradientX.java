dd4f4b134f357a8391ebab4db99dcbcbee633003
haesleinhuepf <rhaase@mpi-cbg.de>
2020-05-14 20:37:01 +0200

    changed default output type to float to be able to hold negative values

--- src/main/java/net/haesleinhuepf/clij2/plugins/GradientX.java
+++ src/main/java/net/haesleinhuepf/clij2/plugins/GradientX.java
@@ -1,6 +1,7 @@
 package net.haesleinhuepf.clij2.plugins;
 
 import net.haesleinhuepf.clij.clearcl.ClearCLBuffer;
+import net.haesleinhuepf.clij.coremem.enums.NativeTypeEnum;
 import net.haesleinhuepf.clij.macro.CLIJMacroPlugin;
 import net.haesleinhuepf.clij.macro.CLIJOpenCLProcessor;
 import net.haesleinhuepf.clij.macro.documentation.OffersDocumentation;
@@ -54,5 +55,10 @@ public class GradientX extends AbstractCLIJ2Plugin implements CLIJMacroPlugin, C
     @Override
     public String getAvailableForDimensions() {
         return "2D, 3D";
+    }
+
+    @Override
+    public ClearCLBuffer createOutputBufferFromSource(ClearCLBuffer input) {
+        return getCLIJ2().create(input.getDimensions(), NativeTypeEnum.Float);
     }
 }
