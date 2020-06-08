69b0d992321b81b95a2547042aadc8d017aacd52
haesleinhuepf <rhaase@mpi-cbg.de>
2020-06-08 11:50:17 +0200

    moved test to clij2-tests repo

--- src/main/java/net/haesleinhuepf/clij2/plugins/GenerateTouchCountMatrix.java
+++ src/main/java/net/haesleinhuepf/clij2/plugins/GenerateTouchCountMatrix.java
@@ -192,29 +192,4 @@ public class GenerateTouchCountMatrix extends AbstractCLIJ2Plugin implements CLI
         return "2D, 3D";
     }
 
-    public static void main(String... args) {
-        CLIJ2 clij2 = CLIJ2.getInstance();
-
-        ClearCLBuffer image1 = clij2.pushString("" +
-                "1 1 0 3\n" +
-                "1 1 2 2\n" +
-                "1 1 2 2\n\n" +
-                "1 1 0 3\n" +
-                "1 1 2 2\n" +
-                "1 1 2 2");
-
-        ClearCLBuffer ref = clij2.pushString("" +
-                "0 2 2 0\n" +
-                "2 16 2 0\n" +
-                "2 2 0 0\n" +
-                "0 0 0 0");
-
-        ClearCLBuffer touch_count_matrix = clij2.create(4, 4);
-
-        GenerateTouchCountMatrix.generateTouchCountMatrix(clij2, image1, touch_count_matrix);
-
-        clij2.print(touch_count_matrix);
-
-
-    }
 }
