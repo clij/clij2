69cd43e4e11bb4c9826b16dfc31b0e43df2e5ad7
haesleinhuepf <rhaase@mpi-cbg.de>
2020-06-08 11:04:34 +0200

    added meanXProjection and meanYProjection

---
+++ src/main/java/net/haesleinhuepf/clij2/plugins/MeanXProjection.java
@@ -0,0 +1,61 @@
+package net.haesleinhuepf.clij2.plugins;
+
+import net.haesleinhuepf.clij.clearcl.ClearCLBuffer;
+import net.haesleinhuepf.clij.clearcl.interfaces.ClearCLImageInterface;
+import net.haesleinhuepf.clij.macro.CLIJMacroPlugin;
+import net.haesleinhuepf.clij.macro.CLIJOpenCLProcessor;
+import net.haesleinhuepf.clij.macro.documentation.OffersDocumentation;
+import net.haesleinhuepf.clij2.AbstractCLIJ2Plugin;
+import net.haesleinhuepf.clij2.CLIJ2;
+import org.scijava.plugin.Plugin;
+
+import java.util.HashMap;
+
+import static net.haesleinhuepf.clij.utilities.CLIJUtilities.assertDifferent;
+
+/**
+ * Author: @haesleinhuepf
+ *         June 2020
+ */
+@Plugin(type = CLIJMacroPlugin.class, name = "CLIJ2_meanXProjection")
+public class MeanXProjection extends AbstractCLIJ2Plugin implements CLIJMacroPlugin, CLIJOpenCLProcessor, OffersDocumentation {
+
+    @Override
+    public boolean executeCL() {
+        return meanXProjection(getCLIJ2(), (ClearCLBuffer)( args[0]), (ClearCLBuffer)(args[1]));
+    }
+
+    public static boolean meanXProjection(CLIJ2 clij2, ClearCLImageInterface src, ClearCLImageInterface dst) {
+        assertDifferent(src, dst);
+
+        HashMap<String, Object> parameters = new HashMap<>();
+        parameters.put("src", src);
+        parameters.put("dst", dst);
+
+        clij2.execute(MeanXProjection.class, "mean_x_projection_x.cl", "mean_x_projection", dst.getDimensions(), dst.getDimensions(), parameters);
+
+        return true;
+    }
+
+    @Override
+    public String getParameterHelpText() {
+        return "Image source, ByRef Image destination";
+    }
+
+    @Override
+    public ClearCLBuffer createOutputBufferFromSource(ClearCLBuffer input)
+    {
+        return getCLIJ2().create(new long[]{input.getWidth(), input.getHeight()}, input.getNativeType());
+    }
+
+    @Override
+    public String getDescription() {
+        return "Determines the mean average intensity projection of an image along X.";
+    }
+
+    @Override
+    public String getAvailableForDimensions() {
+        return "3D -> 2D";
+    }
+
+}
