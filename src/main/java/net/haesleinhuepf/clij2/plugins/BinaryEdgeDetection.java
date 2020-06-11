dd617613491e842d5356995cc82e21b0fabebdb3
haesleinhuepf <rhaase@mpi-cbg.de>
2020-06-11 16:03:50 +0200

    cleanup code

--- src/main/java/net/haesleinhuepf/clij2/plugins/BinaryEdgeDetection.java
+++ src/main/java/net/haesleinhuepf/clij2/plugins/BinaryEdgeDetection.java
@@ -36,7 +36,6 @@ public class BinaryEdgeDetection extends AbstractCLIJ2Plugin implements CLIJMacr
         parameters.put("dst", dst);
 
         clij2.execute(BinaryEdgeDetection.class, "binary_edge_detection_" + dst.getDimension() + "d_x.cl", "binary_edge_detection_" + dst.getDimension() +  "d", dst.getDimensions(), dst.getDimensions(), parameters);
-
         return true;
     }
 
