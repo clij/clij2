2e0516090f93a09eb29fbce886808afd0733166e
haesleinhuepf <robert_haase@gmx.de>
2021-05-20 17:54:31 +0200

    moved euclideanDistanceFromLabelCentroidMap from clijx

---
+++ src/main/java/net/haesleinhuepf/clij2/plugins/EuclideanDistanceFromLabelCentroidMap.java
@@ -0,0 +1,107 @@
+package net.haesleinhuepf.clij2.plugins;
+
+import ij.IJ;
+import ij.ImageJ;
+import ij.ImagePlus;
+import ij.measure.ResultsTable;
+import net.haesleinhuepf.clij.clearcl.ClearCL;
+import net.haesleinhuepf.clij.clearcl.ClearCLBuffer;
+import net.haesleinhuepf.clij.coremem.enums.NativeTypeEnum;
+import net.haesleinhuepf.clij.macro.CLIJMacroPlugin;
+import net.haesleinhuepf.clij.macro.CLIJOpenCLProcessor;
+import net.haesleinhuepf.clij.macro.documentation.OffersDocumentation;
+import net.haesleinhuepf.clij2.AbstractCLIJ2Plugin;
+import net.haesleinhuepf.clij2.CLIJ2;
+import net.haesleinhuepf.clij2.plugins.OnlyzeroOverwriteMaximumBox;
+import net.haesleinhuepf.clij2.plugins.StatisticsOfLabelledPixels;
+import net.haesleinhuepf.clij2.utilities.HasClassifiedInputOutput;
+import net.haesleinhuepf.clij2.utilities.IsCategorized;
+import net.haesleinhuepf.clijx.CLIJx;
+import net.haesleinhuepf.clijx.clij2wrappers.Clear;
+import org.scijava.plugin.Plugin;
+
+import java.util.HashMap;
+
+@Plugin(type = CLIJMacroPlugin.class, name = "CLIJ2_euclideanDistanceFromLabelCentroidMap")
+public class EuclideanDistanceFromLabelCentroidMap extends AbstractCLIJ2Plugin implements CLIJMacroPlugin, CLIJOpenCLProcessor, OffersDocumentation, IsCategorized, HasClassifiedInputOutput {
+    @Override
+    public String getInputType() {
+        return "Label Image";
+    }
+
+    @Override
+    public String getOutputType() {
+        return "Image";
+    }
+
+
+    @Override
+    public String getParameterHelpText() {
+        return "Image labelmap_input, ByRef Image destination";
+    }
+
+    @Override
+    public boolean executeCL() {
+        return euclideanDistanceFromLabelCentroidMap(getCLIJ2(), (ClearCLBuffer) args[0], (ClearCLBuffer) args[1]);
+    }
+
+    public static boolean euclideanDistanceFromLabelCentroidMap(CLIJ2 clij2, ClearCLBuffer pushed, ClearCLBuffer result) {
+        int number_of_labels = (int)clij2.maximumOfAllPixels(pushed);
+        ClearCLBuffer pointlist = clij2.create(number_of_labels + 1,pushed.getDimension(), 1);
+
+        clij2.centroidsOfBackgroundAndLabels(pushed, pointlist);
+
+        HashMap<String, Object> parameters = new HashMap();
+        parameters.put("src", pushed);
+        parameters.put("pointlist", pointlist);
+        parameters.put("dst", result);
+        clij2.execute(EuclideanDistanceFromLabelCentroidMap.class, "euclidean_distance_from_label_centroid_map_x.cl", "euclidean_distance_from_label_centroid_map", result.getDimensions(), result.getDimensions(), parameters);
+
+        pointlist.close();
+
+        return true;
+    }
+
+    @Override
+    public ClearCLBuffer createOutputBufferFromSource(ClearCLBuffer input)
+    {
+        return getCLIJ2().create(input.getDimensions(), NativeTypeEnum.Float);
+    }
+
+    @Override
+    public String getDescription() {
+        return "Takes a label map, determines the centroids of all labels and writes the distance of all labelled pixels to their centroid in the result image.\nBackground pixels stay zero.";
+    }
+
+    @Override
+    public String getAvailableForDimensions() {
+        return "2D, 3D";
+    }
+
+    @Override
+    public String getCategories() {
+        return "Label, Measurements";
+    }
+
+    public static void main(String[] args) {
+        new ImageJ();
+
+        ImagePlus imp = IJ.openImage("src/test/resources/blobs.tif");
+
+        CLIJx clijx = CLIJx.getInstance();
+
+        ClearCLBuffer input = clijx.push(imp);
+        ClearCLBuffer binary = clijx.create(input);
+        ClearCLBuffer labels = clijx.create(input);
+        ClearCLBuffer edcm = clijx.create(input.getDimensions(), input.getNativeType());
+
+        clijx.thresholdOtsu(input, binary);
+        clijx.connectedComponentsLabelingBox(binary, labels);
+
+        euclideanDistanceFromLabelCentroidMap(clijx, labels, edcm);
+
+        clijx.show(edcm, "edcm");
+
+
+    }
+}
