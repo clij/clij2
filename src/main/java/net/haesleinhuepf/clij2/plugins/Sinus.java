2a001df4ac36a9b22dbe17fcda339506639c306f
haesleinhuepf <robert_haase@gmx.de>
2021-05-20 15:22:09 +0200

    moved in correct folder

--- src/main/java/net/haesleinhuepf/clij2/plugins/Sinus.java
+++ src/main/java/net/haesleinhuepf/clij2/plugins/Sinus.java
@@ -1,4 +1,4 @@
-package net.haesleinhuepf.clij2;
+package net.haesleinhuepf.clij2.plugins;
 
 import net.haesleinhuepf.clij.clearcl.ClearCLBuffer;
 import net.haesleinhuepf.clij.clearcl.interfaces.ClearCLImageInterface;
