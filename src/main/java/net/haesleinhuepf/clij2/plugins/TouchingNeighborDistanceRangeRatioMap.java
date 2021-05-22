a4439d0dfb57077a069906e9f2fa210ac0e236f0
haesleinhuepf <robert_haase@gmx.de>
2021-05-22 09:07:01 +0200

    corrected names

--- src/main/java/net/haesleinhuepf/clij2/plugins/TouchingNeighborDistanceRangeRatioMap.java
+++ src/main/java/net/haesleinhuepf/clij2/plugins/TouchingNeighborDistanceRangeRatioMap.java
@@ -11,7 +11,7 @@ import net.haesleinhuepf.clij2.utilities.HasClassifiedInputOutput;
 import net.haesleinhuepf.clij2.utilities.IsCategorized;
 import org.scijava.plugin.Plugin;
 
-@Plugin(type = CLIJMacroPlugin.class, name = "CLIJx_touchingNeighborDistanceRangeRatioMap")
+@Plugin(type = CLIJMacroPlugin.class, name = "CLIJ2_touchingNeighborDistanceRangeRatioMap")
 public class TouchingNeighborDistanceRangeRatioMap extends AbstractCLIJ2Plugin implements CLIJMacroPlugin, CLIJOpenCLProcessor, OffersDocumentation, IsCategorized, HasClassifiedInputOutput {
     @Override
     public String getInputType() {
